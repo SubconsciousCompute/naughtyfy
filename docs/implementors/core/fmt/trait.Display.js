f8ed63e692d41c595a8650f7bd9ccdfdc08215f1
Tanveer Raza <tanveerraza789@gmail.com>
2023-02-24 11:16:43 +0530

    Add docs

---
+++ docs/implementors/core/fmt/trait.Display.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"naughtyfy":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"naughtyfy/errors/struct.FanotifyInitError.html\" title=\"struct naughtyfy::errors::FanotifyInitError\">FanotifyInitError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
