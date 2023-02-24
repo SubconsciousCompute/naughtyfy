4ee857124be0ab719e7862ffa0e6f8d79efbb14a
Tanveer Raza <tanveerraza789@gmail.com>
2023-02-24 11:08:33 +0530

    Add readme, update example, change file structure.

---
+++ README.md
@@ -0,0 +1,6 @@
+# 📁 Naughtyfy
+A modern fanotify wrapper
+
+Currently in development stage but example is working
+
+build example using `cargo build --release --example lowkey` and run using `sudo ./target/release/examples/lowkey`
--- src/low_api.rs
+++ src/low_api.rs
@@ -1,79 +1,11 @@
 use lazy_static::lazy_static;
-use libc::{__s32, __u16, __u32, __u64, __u8, c_int};
+
+use crate::structs::*;
 use std::{io::Error, mem, os::unix::ffi::OsStrExt, slice};
 
 lazy_static! {
     /// Get current platform sizeof of fanotify_event_metadata.
     pub static ref FAN_EVENT_METADATA_LEN: usize = mem::size_of::<fanotify_event_metadata>();
-}
-
-/// After a successful read(2), the read buffer contains the following structure
-#[derive(Debug, Clone, Copy)]
-#[repr(C)]
-pub struct fanotify_event_metadata {
-    /// This is the length of the data for the current event and the
-    /// offset to the next event in the buffer.  Without
-    /// FAN_REPORT_FID, the value of event_len is always
-    /// FAN_EVENT_METADATA_LEN.  With FAN_REPORT_FID, event_len also
-    /// includes the variable length file identifier.
-    pub event_len: __u32,
-    /// This field holds a version number for the structure.  It must
-    /// be compared to FANOTIFY_METADATA_VERSION to verify that the
-    /// structures returned at run time match the structures defined
-    /// at compile time.  In case of a mismatch, the application
-    /// should abandon trying to use the fanotify file descriptor.
-    pub vers: __u8,
-    /// This field is not used.
-    pub reserved: __u8,
-    /// This is the length of the structure.  The field was introduced
-    /// to facilitate the implementation of optional headers per event
-    /// type.  No such optional headers exist in the current implemen‐
-    /// tation.
-    pub metadata_len: __u16,
-    /// This is a bit mask describing the event (see below).
-    pub mask: __u64,
-    /// This is an open file descriptor for the object being accessed,or FAN_NOFD if a queue overflow occurred.  
-    /// If the fanotify file descriptor has been initialized using FAN_REPORT_FID,
-    /// applications should expect this value to be set to FAN_NOFDfor each event that is received.  The file descriptor can be
-    /// used to access the contents of the monitored file or directory.  The reading application is responsible for closing this file descriptor.
-    /// When calling fanotify_init(2), the caller may specify (via the event_f_flags argument) various file status flags that are to
-    /// be set on the open file description that corresponds to this file descriptor.  In addition, the (kernel-internal) FMODE_NONOTIFY file status flag is set on the open file description.  
-    /// This flag suppresses fanotify event generation. Hence, when the receiver of the fanotify event accesses the notified file or directory using this file descriptor, noadditional events will be created.
-    pub fd: __s32,
-    /// If flag FAN_REPORT_TID was set in fanotify_init(2), this is
-    /// the TID of the thread that caused the event.  Otherwise, this
-    /// the PID of the process that caused the event.
-    pub pid: __s32,
-}
-
-#[derive(Debug, Clone, Copy)]
-#[repr(C)]
-pub struct fanotify_event_info_header {
-    pub info_type: __u8,
-    pub pad: __u8,
-    pub len: __u16,
-}
-
-#[derive(Debug, Clone, Copy)]
-#[allow(non_camel_case_types)]
-struct __kernel_fsid_t {
-    val: [c_int; 2],
-}
-
-#[derive(Debug, Clone, Copy)]
-#[repr(C)]
-struct fanotify_event_info_fid {
-    hdr: fanotify_event_info_header,
-    fsid: __kernel_fsid_t,
-    file_handle: __u8,
-}
-
-#[derive(Debug)]
-#[repr(C)]
-/// It is used to control file access.
-pub struct fanotify_response {
-    pub fd: __s32,
-    pub response: __u32,
 }
 
 /// Initializes a new fanotify group and returns a file descriptor for the event queue associated with the group.<br/>
