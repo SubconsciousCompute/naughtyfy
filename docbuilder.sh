ce1ef20f9eaebc6be5c8b7e29f8216b63409eeaa
Tanveer Raza <tanveerraza789@gmail.com>
2023-02-25 19:05:12 +0530

    Highly document fanotify_init() and _mark(). Added suitable flags.

--- docbuilder.sh
+++ docbuilder.sh
@@ -1,4 +1,4 @@
 cargo doc --no-deps
 rm -rf ./docs
-echo "<meta http-equiv=\"refresh\" content=\"0; url=naughtyfy\">" > target/doc/index.html
+echo "<meta http-equiv=\"refresh\" content=\"0; url=naughtyfy\">" >target/doc/index.html
 cp -r target/doc ./docs
