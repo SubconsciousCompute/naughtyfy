de3b9cd1de3290aa06957ec15806760c979f2b74
tanveerraza789 <tanveerraza789@gmail.com>
2023-02-27 23:22:21 +0530

    Add mods desc

--- examples/errtest.rs
+++ examples/errtest.rs
@@ -3,8 +3,9 @@ use naughtyfy::api::*;
 use naughtyfy::flags::*;
 
 fn main() {
+    // This will throw error
     unsafe {
-        FAN_EVENT_BUFFER_LEN = 230;
+        FAN_EVENT_BUFFER_LEN = 999999999;
     }
     let fd = fanotify_init(FAN_CLASS_NOTIF, 0).unwrap();
     fanotify_mark(fd, FAN_MARK_ADD | FAN_MARK_MOUNT, FAN_ACCESS, AT_FDCWD, "/").unwrap();
